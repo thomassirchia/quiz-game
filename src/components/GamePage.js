2ae80e921524057d3b6f1780e16154c6d5b8ff41
ThomasSirchia <thomassirchia@icloud.com>
2023-03-02 15:52:44 -0800

    Improve mobile styling

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -29,8 +29,7 @@ export default function GamePage(props) {
     numberSelectedAnswers === props.questions.length;
 
   return (
-    <div className="game-page-container card-effect">
-      {/* <img src="images/circle-arrow-left.svg" alt="Back Button"></img> */}
+    <div className="game-page-container">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 448 512"
@@ -39,41 +38,43 @@ export default function GamePage(props) {
       >
         <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
       </svg>
-      <h1 className="game-page-title">TriviaQuiz</h1>
-      <h3 className="game-page-tagline">
-        {props.numQuestions} {props.categoryName} Trivia Questions
-      </h3>
-      {/* <Settings
+      <div className="game-page-card card-effect">
+        <h1 className="game-page-title">TriviaQuiz</h1>
+        <h3 className="game-page-tagline">
+          {props.numQuestions} {props.categoryName} Trivia Questions
+        </h3>
+        {/* <Settings
         numQuestions={props.numQuestions}
         setNumQuestions={props.setNumQuestions}
         category={props.category}
         setCategory={props.setCategory}
       /> */}
-      {questionElements}
+        {questionElements}
 
-      {props.checkAnswers && (
-        <div className="score-container">
-          <p className="score">
-            You scored {props.score}/{props.questions.length} correct answers
-          </p>
+        {props.checkAnswers && (
+          <div className="score-container">
+            <p className="score">
+              You scored {props.score}/{props.questions.length} correct answers
+            </p>
+            <button
+              className="btn btn-play-again btn-grow"
+              onClick={props.handlePlayAgain}
+            >
+              Play Again
+            </button>
+          </div>
+        )}
+
+        {!props.checkAnswers && (
           <button
-            className="btn btn-play-again btn-grow"
-            onClick={props.handlePlayAgain}
+            className="btn btn-check btn-grow"
+            onClick={props.handleCheckAnswers}
+            disabled={!userAnsweredAllQuestions}
           >
-            Play Again
+            Check Answers
           </button>
-        </div>
-      )}
-
-      {!props.checkAnswers && (
-        <button
-          className="btn btn-check btn-grow"
-          onClick={props.handleCheckAnswers}
-          disabled={!userAnsweredAllQuestions}
-        >
-          Check Answers
-        </button>
-      )}
+        )}
+      </div>
     </div>
   );
 }
--- src/components/StartGame.js
+++ src/components/StartGame.js
@@ -5,24 +5,26 @@ export default function StartGame(props) {
   const categoryName = categoryNames[props.category];
 
   return (
-    <div className="start-game card-effect">
-      <h1 className="start-game-title">TriviaQuiz</h1>
-      <Settings
-        numQuestions={props.numQuestions}
-        setNumQuestions={props.setNumQuestions}
-        category={props.category}
-        setCategory={props.setCategory}
-      />
-      {/* <h1 className="start-game-title">Start Game</h1> */}
-      <h3 className="start-game-tagline">
-        You will get {props.numQuestions} {categoryName} trivia questions
-      </h3>
-      <button
-        className="btn btn-start-game btn-grow"
-        onClick={props.handleStartGame}
-      >
-        Start Game
-      </button>
+    <div className="start-game-container">
+      <div className="start-game-card card-effect">
+        <h1 className="start-game-title">TriviaQuiz</h1>
+        <Settings
+          numQuestions={props.numQuestions}
+          setNumQuestions={props.setNumQuestions}
+          category={props.category}
+          setCategory={props.setCategory}
+        />
+        {/* <h1 className="start-game-title">Start Game</h1> */}
+        <h3 className="start-game-tagline">
+          You will get {props.numQuestions} {categoryName} trivia questions
+        </h3>
+        <button
+          className="btn btn-start-game btn-grow"
+          onClick={props.handleStartGame}
+        >
+          Start Game
+        </button>
+      </div>
     </div>
   );
 }
